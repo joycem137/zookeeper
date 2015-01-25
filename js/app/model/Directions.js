ad238b4fbe01274a9318512b962e165a9c15c207
Joyce Murton <joycem137@gmail.com>
2015-01-24 18:28:20 -0800

    Fixed Directions enum to be smaller.

--- js/app/model/Directions.js
+++ js/app/model/Directions.js
@@ -1,12 +1,9 @@
 /*
  * Directions
  */
-define([],
-function () {
-    return {
-        NORTH: 0,
-        EAST: 1,
-        SOUTH: 2,
-        WEST: 3
-    };
+define({
+    NORTH: 0,
+    EAST: 1,
+    SOUTH: 2,
+    WEST: 3
 });
